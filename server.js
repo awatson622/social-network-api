6a4707841a91a7ed47a0c383e7926bb9c5e69f95
Alexis Watson <alexiswatson622@hotmail.com>
2024-05-18 11:44:54 -0700

    attempted to fix bug in code. failed.

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/social-network-api';
-
 const app = express();
 
 app.use(express.json());
@@ -12,11 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-mongoose.connect(MONGODB_URI, {
+const uri = "mongodb+srv://awatson622:aHvn3EinBHl2IzRw@cluster00163.vh7clrz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster00163";
+
+mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
 });
 
 mongoose.connection.on('connected', () => {
@@ -26,34 +24,6 @@ mongoose.connection.on('connected', () => {
 mongoose.connection.on('error', (err) => {
   console.error('MongoDB connection error:', err);
 });
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://awatson622: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@cluster00163.mongodb.net/social-network-api?retryWrites=true&w=majority";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
